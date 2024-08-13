98c0711e4e41273505db89a149fbba8987a1f145
Neemias Calebe <neemiascalebe2014@gmail.com>
2024-08-13 19:31:54 -0300

    Fix: Falha ao baixar pdf

--- cardapio.py
+++ cardapio.py
@@ -4,36 +4,35 @@ import re
 
 class Cardapio:
     def __init__(self) -> None:
-        self.base_url = "http://ufpi.br"
+        self.base_url = "https://ufpi.br"
         self.page_url = f"{self.base_url}/restaurante-universitario"
         self.response = None
-        self.status = None
+        self.pdf_url = None
+        self.filename = "Download/cardapio.pdf"
     
     def get(self):
         self.response = requests.get(self.page_url)
-        if self.response.status_code == 200:
-            soup = BeautifulSoup(self.response.content, "html.parser")
-            pdf_links = soup.find_all("a", string=re.compile("^Confira o cardápio em Teresina"))
-            if pdf_links:
-                pdf_link = pdf_links[0]["href"]
-                pdf_url = f"{self.base_url}{pdf_link}"
-
-                """ pdf_response = requests.get(pdf_url)
-                if pdf_response.status_code == 200:
-                    filename = "Out/cardapio.pdf"
-                    with open(filename, "wb") as file:
-                        file.write(pdf_response.content)
-                    
-                    self.status = f"Download do cardápio concluído com sucesso. Arquivo salvo como {filename}."
-                else:
-                    self.status = "Erro ao baixar o cardápio." """
-            else:
-                self.status = "Link do cardápio não encontrado."
-        else:
-            self.status = "Erro ao acessar a página do cardápio."
         
-        return pdf_url
+        if self.response.status_code != 200:
+            return None, self.response.status_code
+        
+        soup = BeautifulSoup(self.response.content, "html.parser")
+        pdf_links = soup.find_all("a", string=re.compile("^Confira o cardápio em Teresina"))
+        
+        if not pdf_links:
+            return None, "Erro ao resgatar cardápio :("
+            
+        pdf_link = pdf_links[0]["href"]
+        self.pdf_url = f"{self.base_url}{pdf_link}"
+        
+        pdf_response = requests.get(self.pdf_url)
+        if pdf_response.status_code != 200:
+            return None, pdf_response.status_code
+        
+        with open(self.filename, "wb") as file:
+            file.write(pdf_response.content)
+            
+        return self.filename, None
 
 if __name__ == "__main__":
     c = Cardapio()
-    c.get()
